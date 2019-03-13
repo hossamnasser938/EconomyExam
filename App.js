974f6b04a35ecda52e2e93f28d423e3fc455f856
Hosam <hossamnasser938@gmail.com>
2019-03-13 15:05:19 +0200

    Move to training name instead of learning

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { Navigation } from 'react-native-navigation';
 import ContentScreen from './src/screens/MainTabs/Content/Content';
-import TrainingScreen from './src/screens/MainTabs/Learning/Training';
+import TrainingScreen from './src/screens/MainTabs/Training/Training';
 import ContentQuestionScreen from './src/screens/Question/ContentQuestion';
 import TrainQuestionScreen from './src/screens/Question/TrainQuestion';
 import startMainTabs from './src/screens/MainTabs/startMainTabs';

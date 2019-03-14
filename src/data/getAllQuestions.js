7d78822cfe41970859d16363b0de0cc78f676135
Hosam <hossamnasser938@gmail.com>
2019-03-14 21:45:27 +0200

    Working script to get all files

--- src/data/getAllQuestions.js
+++ src/data/getAllQuestions.js
@@ -2,11 +2,18 @@ import RNFS from 'react-native-fs';
 
 const getAllQuestions = () => {
     RNFS.readDirAssets("chapters")
-    .then(result => {
-        console.log( "result:", result );
-    }).catch( reason => {
-        console.log( "error due to:", reason );  
+    .then(results => {
+        console.log( "result:", results );
+        const files = results.map( result => result.name );
+        const promises = files.map( file => RNFS.readFileAssets('chapters/' + file) );
+        return Promise.all( promises );
+    } )
+    .then(contents => {
+        console.log( "contents:", contents )
     })
+    .catch( reason => {
+        console.log( "error due to:", reason );  
+    });
 };
 
 export default getAllQuestions;

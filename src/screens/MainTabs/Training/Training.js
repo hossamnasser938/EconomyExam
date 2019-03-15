b0e008aba479c4e6ec23c4b11b1367391a41b732
Hosam <hossamnasser938@gmail.com>
2019-03-15 21:05:43 +0200

    Split training screen code

--- src/screens/MainTabs/Training/Training.js
+++ src/screens/MainTabs/Training/Training.js
@@ -1,53 +1,13 @@
 import React, { Component } from 'react';
 import { View, Button } from 'react-native';
 import styles from './style';
-import getAllQuestions from '../../../data/getAllQuestions';
-import preprocessQuestions from '../../../data/preprocessQuestions';
-import shuffle from '../../../data/shuffleArray';
-import Papa from 'papaparse';
-import RNFS from 'react-native-fs';
+import startTrainingHandler from './startTrainingHandler';
 
 
 class TrainingScreen extends Component {
     onStartTraining = () => {
-        console.log("lets get questions together");
-        getAllQuestions()
-        .then(results => {
-            console.log( "result:", results );
-            const files = results.map( result => result.name );
-            const promises = files.map( file => RNFS.readFileAssets('chapters/' + file) );
-            return Promise.all( promises );
-        } )
-        .then(contents => {
-            console.log( "contents:", contents )
-
-            const chaptersQuestions = contents.
-                map( content => Papa.parse( content ).data )
-            console.log( "chaptersQuestions:", chaptersQuestions );
-
-            const filteredChaptersQuestions = chaptersQuestions.
-                map( chaptersQuestions => preprocessQuestions( chaptersQuestions ) )
-            console.log( "filteredChaptersQuestions:", filteredChaptersQuestions );
-
-            const combinedQuestions = filteredChaptersQuestions.
-                reduce( ( accumulator, singleChapterQuestions ) => 
-                    accumulator.concat( singleChapterQuestions ), [] );
-            console.log("combinedQuestions:", combinedQuestions);
-
-            const shuffledQuestions = shuffle( combinedQuestions );
-            console.log( "shuffledQuestions:", shuffledQuestions );
-
-            this.props.navigator.push( {
-                screen: "EconomyExam.TrainQuestionScreen",
-                title: "Question",
-                passProps: {
-                    questions: shuffledQuestions
-                }
-            } );
-        })
-        .catch( reason => {
-            console.log( "error due to:", reason );  
-        });
+        const validStartTrainingHandler = startTrainingHandler.bind( this );
+        validStartTrainingHandler();
     };
     
     render() {
---
+++ src/screens/MainTabs/Training/startTrainingHandler.js
@@ -0,0 +1,49 @@
+import getAllQuestions from '../../../data/getAllQuestions';
+import preprocessQuestions from '../../../data/preprocessQuestions';
+import shuffle from '../../../data/shuffleArray';
+import Papa from 'papaparse';
+import RNFS from 'react-native-fs';
+
+function startTrainingHandler() {
+    console.log( "from start training handler" );
+
+    getAllQuestions()
+        .then(results => {
+            console.log( "result:", results );
+            const files = results.map( result => result.name );
+            const promises = files.map( file => RNFS.readFileAssets('chapters/' + file) );
+            return Promise.all( promises );
+        } )
+        .then(contents => {
+            console.log( "contents:", contents )
+
+            const chaptersQuestions = contents.
+                map( content => Papa.parse( content ).data )
+            console.log( "chaptersQuestions:", chaptersQuestions );
+
+            const filteredChaptersQuestions = chaptersQuestions.
+                map( chaptersQuestions => preprocessQuestions( chaptersQuestions ) )
+            console.log( "filteredChaptersQuestions:", filteredChaptersQuestions );
+
+            const combinedQuestions = filteredChaptersQuestions.
+                reduce( ( accumulator, singleChapterQuestions ) => 
+                    accumulator.concat( singleChapterQuestions ), [] );
+            console.log("combinedQuestions:", combinedQuestions);
+
+            const shuffledQuestions = shuffle( combinedQuestions );
+            console.log( "shuffledQuestions:", shuffledQuestions );
+
+            this.props.navigator.push( {
+                screen: "EconomyExam.TrainQuestionScreen",
+                title: "Question",
+                passProps: {
+                    questions: shuffledQuestions
+                }
+            } );
+        })
+        .catch( reason => {
+            console.log( "error due to:", reason );  
+        });
+}
+
+export default startTrainingHandler;

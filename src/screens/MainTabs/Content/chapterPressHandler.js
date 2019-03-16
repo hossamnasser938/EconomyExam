4d94d81a9234525f1f39df34e5c854cae6ba579a
Hosam <hossamnasser938@gmail.com>
2019-03-16 07:54:46 +0200

    Improve Content Modal

--- src/screens/MainTabs/Content/chapterPressHandler.js
+++ src/screens/MainTabs/Content/chapterPressHandler.js
@@ -4,21 +4,21 @@ import Papa from 'papaparse';
 
 function chapterPressHandler ( chapter ) {
     console.log( "from chapter press handler with modal" );
-    const chapterQuestionsPromise = getChapterQuestions( chapter );
-
     this.props.navigator.showModal( {
         screen: "EconomyExam.LoadingModalScreen",
         animationType: "none"
     } );
 
+    const chapterQuestionsPromise = getChapterQuestions( chapter );
+
     chapterQuestionsPromise.then( result => {
         const questions = Papa.parse( result ).data;
-        
+
+        const filteredQuestions = preprocessQuestions( questions )
+
         this.props.navigator.dismissModal( {
             animationType: "none"
         } );
-
-        const filteredQuestions = preprocessQuestions( questions )
 
         this.props.navigator.push( {
             screen: "EconomyExam.ContentQuestionScreen",

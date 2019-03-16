556392fce64f027106152bc645c37b0b4e6baf39
Hosam <hossamnasser938@gmail.com>
2019-03-16 07:50:07 +0200

    Improve loading Modal

--- src/screens/LoadingModal/LoadingModal.js
+++ src/screens/LoadingModal/LoadingModal.js
@@ -2,12 +2,38 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import styles from './styles';
 
+const loadingPatterns = [".", ". .", ". . ."];
+
+var interval;
+
 class LoadingModal extends Component {
+    constructor( props ) {
+        super( props );
+        this.state = {
+            currentPattern: 0
+        };
+        interval = setInterval(
+            () => {
+                this.setState( prevState => {
+                    return {
+                        currentPattern: (prevState.currentPattern + 1) % 3
+                    }
+                } );
+            },
+            100            
+        );
+    }
+
+    componentWillUnmount() {
+        console.log( "componentWillUnmount" );
+        clearInterval( interval );
+    }
+
     render() {
         console.log( "render loading modal" )
         return (
             <View style = { styles.container }>
-                <Text style = { styles.loadingText }>Loading ...</Text>
+                <Text style = { styles.loadingText }>Loading { loadingPatterns[this.state.currentPattern] }</Text>
             </View>
         );
     }

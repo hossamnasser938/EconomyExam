dad3d87df0fc00fcebe54294368afdbfdba4417f
Hosam <hossamnasser938@gmail.com>
2019-04-24 22:14:31 +0200

    provide guidelines for user

--- src/components/AuthInput/AuthInput.js
+++ src/components/AuthInput/AuthInput.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import { View, TextInput } from 'react-native';
+import { View, TextInput, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { LESS_DARK_TEXT_COLOR, DARK_TEXT_COLOR } from '../../utils/colors';
 import styles from './styles';
 
 const AuthInput = props => {
   let key = null;
+  let hint = null;
   switch( props.placeholder ) {
     case "E-mail":
       key = "email";
+      hint = "Enter a valid email e.g example@example.com";
       break;
     case "Name":
       key = "name";
+      hint = "Enter a valid name that contains alphabets e.g Mohamed Ali";
       break;
     case "Password":
       key = "password";
+      hint = "Enter a valid password of at least 6 characters";
       break;
     case "Confirm Password":
       key = "confirmPassword";
+      hint = "repeat password";
       break;
   }
 
@@ -43,11 +48,19 @@ const AuthInput = props => {
             />
 
             <View style = { styles.checkMarkWrapper }>
-                <Icon
-                  name = { props.valid? "check": "information-outline"}
-                  size = { 20 }
-                  color = { DARK_TEXT_COLOR }
-                />
+                <TouchableOpacity 
+                  onPress = { 
+                              props.valid
+                              ? () => console.log( "Valid", props.placeholder )
+                              : () => { alert( hint ) } 
+                            }
+                >
+                  <Icon
+                    name = { props.valid? "check": "information-outline"}
+                    size = { 20 }
+                    color = { DARK_TEXT_COLOR }
+                  />
+                </TouchableOpacity>
             </View>
         </View>
     </View>

36897a8ed8d2eb22b32810661001ac625b87750a
Hosam <hossamnasser938@gmail.com>
2019-04-25 14:26:12 +0200

    configure redux debugging

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,11 +1,18 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import testReducer from './reducers/test';
 
 const rootReducer = combineReducers( { test: testReducer } );
 
+let composeEnhancers = compose;
+
+if ( __DEV__ ) {
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
+
 const configStore = () => {
     return createStore( 
-        rootReducer
+        rootReducer,
+        composeEnhancers()
      );
 };
 
